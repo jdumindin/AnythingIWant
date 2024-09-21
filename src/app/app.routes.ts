301a8659318bd7c102b0770939c09a962da77fe4
jndumindin <jndumindin@gmail.com>
2024-09-20 17:26:37 -0700

    9/20/2024 5:26pm

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { CityComponent } from './city/city.component';
+import { CountryComponent } from './country/country.component';
+import { WeatherComponent } from './weather/weather.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'cities', component:CityComponent},
+    {path:'countries', component:CountryComponent},
+    {path:'', component:WeatherComponent, pathMatch:'full'}
+];
